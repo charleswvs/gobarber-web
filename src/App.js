88a2725dbe128a0da130a5a417249b0c6ffa8acf
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 12:17:59 -0300

    Add react-toastify for error messages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -20,6 +21,7 @@ function App() {
         <Router history={history}>
           <Routes />
           <GlobalStyles />
+          <ToastContainer autoClose={4000} />
         </Router>
       </PersistGate>
     </Provider>
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import { signInSuccess, signFailure } from './actions';
@@ -16,13 +17,14 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
     console.tron.log(user);
     if (!user.provider) {
-      console.tron.error('User is not a provider');
+      toast.error('User is not a provider');
       return;
     }
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Authentication error, please verify you info');
     yield put(signFailure());
   }
 }
