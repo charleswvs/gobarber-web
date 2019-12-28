277171306684c9c46dcab0607040c7945fcab608
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 12:12:27 -0300

    add loading and failure action

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,26 +1,30 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import api from '~/services/api';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
-  console.tron.log(user);
-  if (!user.provider) {
-    console.tron.error('User is not a provider');
-    return;
+    const { token, user } = response.data;
+    console.tron.log(user);
+    if (!user.provider) {
+      console.tron.error('User is not a provider');
+      return;
+    }
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
