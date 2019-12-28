fc2446cb972f5a06959953af135fe315ff92becf
Charles Willian <wcharlesw15@gmail.com>
2019-12-27 21:07:20 -0300

    Configured routes redirecting and private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -11,8 +12,13 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+
+      {/* private routes can only be accessed if the params inside the route have been met */}
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+
+      {/* Could be used for a 404 error */}
+      {/* <Route path="/" component={() => <h1>404 not found</h1>} /> */}
     </Switch>
   );
 }
