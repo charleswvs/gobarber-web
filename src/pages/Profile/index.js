9babc445d36e5b066d639fb244a2c360fc0a4955
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 17:16:19 -0300

    created Profile page base and styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your email address" />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input type="password" name="password" placeholder="New Password" />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Confirm new password"
+        />
+
+        <button type="submit">Update Profile</button>
+      </Form>
+
+      <button type="button">Exit GoBarber</button>
+    </Container>
+  );
 }
