285a762415b4469e4488924b25652dcb6a935a14
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 09:58:40 -0300

    redux & redux sagas initial config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store'; // store must be imported after Reactotron, or else there will be problems
+
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 }
 
