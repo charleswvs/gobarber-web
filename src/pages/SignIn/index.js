5332d960b0e9ed65f0b381299977dc876261c172
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 09:31:36 -0300

    Validation with Yup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/images/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insert a valid email')
+    .required("Email can't be empty"),
+  password: Yup.string().required("Password can't be empty"),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -13,7 +21,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="GoBarber-Logo" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="emal" placeholder="Your email" />
         <Input name="password" type="password" placeholder="Your password" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/images/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("Name can't be empty"),
+  email: Yup.string()
+    .email('Insert a valid email')
+    .required("Email can't be empty"),
+  password: Yup.string()
+    .min(6, 'Password must be longer than 6 characters')
+    .required("Password can't be empty"),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -13,7 +24,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarber-Logo" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="emal" placeholder="Your email" />
         <Input name="password" type="password" placeholder="Your password" />
