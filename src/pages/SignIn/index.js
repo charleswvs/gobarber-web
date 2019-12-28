47d850933e2b93f8f603c2534da695b8046eed8c
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 09:18:37 -0300

    add unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/images/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber-Logo" />
 
-      <form>
-        <input type="emal" placeholder="Your email" />
-        <input type="password" placeholder="Your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="emal" placeholder="Your email" />
+        <Input name="password" type="password" placeholder="Your password" />
 
         <button type="submit">SignIn</button>
 
         <Link to="/register">SignUp for a free account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/images/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber-Logo" />
 
-      <form>
-        <input placeholder="Full name" />
-        <input type="emal" placeholder="Your email" />
-        <input type="password" placeholder="Your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="emal" placeholder="Your email" />
+        <Input name="password" type="password" placeholder="Your password" />
 
         <button type="submit">SignUn</button>
 
         <Link to="/">Have an account? SingIn</Link>
-      </form>
+      </Form>
     </>
   );
 }
