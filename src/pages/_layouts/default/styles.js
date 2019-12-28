0e5657d632c994dcbe626ae0045b9ef838633287
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 14:27:22 -0300

    logged in header styled

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #ccc;
+  background: linear-gradient(-90deg, #7159c1, #ab59c1);
   /* background: linear-gradient(-90deg, #7159c1, #ab59c1); */
 `;
