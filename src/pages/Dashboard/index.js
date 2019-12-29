9ac3d84f323c30ba1e0a3ca3f2e0c808c15842b9
Charles Willian <wcharlesw15@gmail.com>
2019-12-28 22:10:36 -0300

    Created and styled dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
+import { format, subDays, addDays } from 'date-fns';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(() => format(date, 'MMMM d'), [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Charles Willian</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Charles Willian</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Charles Willian</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Charles Willian</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
